c11beff9aa520a1b5cc820b83d69c41102132db9
Hernan Aldunate <hernan_aldunate@yahoo.com.ar>
2020-09-28 14:08:19 -0300

    Image resizing in detail view

--- Top50Challenge/EntryDetailViewController.swift
+++ Top50Challenge/EntryDetailViewController.swift
@@ -13,15 +13,57 @@ class EntryDetailViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var pictureWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pictureHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var titleLabel: UILabel!
 
-    var dataTask: URLSessionDataTask?
+    private var dataTask: URLSessionDataTask?
+    private var originalImageSize: CGSize?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scrollView.isHidden = true
         loadingLabel.isHidden = false
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        if pictureImageView.image != nil {
+            resizeImage(for: scrollView.bounds.width)
+        }
+    }
+
+    private func setImage(_ image: UIImage) {
+        pictureImageView.isHidden = false
+        pictureImageView.image = image
+        resizeImage(for: scrollView.bounds.width)
+    }
+
+    private func resetImage() {
+        pictureImageView.isHidden = true
+        pictureImageView.image = nil
+        pictureHeightConstraint.constant = 0
+    }
+
+    private func resizeImage(for containerWidth: CGFloat) {
+        let imageClippedSize = contentClippingRect(for: containerWidth)
+        pictureWidthConstraint.constant = imageClippedSize.width
+        pictureHeightConstraint.constant = imageClippedSize.height
+    }
+
+    private func contentClippingRect(for containerWidth: CGFloat) -> CGRect {
+        guard let originalImageSize = originalImageSize, originalImageSize.width > 0 && originalImageSize.height > 0 else {
+            return CGRect.zero
+        }
+
+        let scale = min(containerWidth / originalImageSize.width, 1)
+
+        let newSize = CGSize(width: originalImageSize.width * scale, height: originalImageSize.height * scale)
+        let x = (containerWidth - newSize.width) * 0.5
+
+        return CGRect(x: x, y: 0, width: newSize.width, height: newSize.height)
     }
 }
 
@@ -33,8 +75,7 @@ extension EntryDetailViewController: EntrySelectionDelegate {
         loadingLabel.isHidden = true
         authorLabel.text = author
         titleLabel.text = title
-        pictureImageView.image = nil
-        pictureImageView.isHidden = true
+        resetImage()
 
         dataTask?.suspend()
         if let pictureURL = pictureURL {
@@ -43,16 +84,27 @@ extension EntryDetailViewController: EntrySelectionDelegate {
                     return
                 }
 
-                self.pictureImageView.isHidden = false
-                self.pictureImageView.image = image
+                self.originalImageSize = image.size
+                self.setImage(image)
             }, failure: { [weak self] error in
-                self?.pictureImageView.isHidden = thumbnail == nil
-                self?.pictureImageView.image = thumbnail
-                print(error)
+                guard let self = self else {
+                    return
+                }
+
+                if let thumbnail = thumbnail {
+                    self.originalImageSize = thumbnail.size
+                    self.setImage(thumbnail)
+                } else {
+                    self.resetImage()
+                }
             })
         } else {
-            pictureImageView.isHidden = thumbnail == nil
-            pictureImageView.image = thumbnail
+            if let thumbnail = thumbnail {
+                originalImageSize = thumbnail.size
+                setImage(thumbnail)
+            } else {
+                resetImage()
+            }
         }
     }
 }
